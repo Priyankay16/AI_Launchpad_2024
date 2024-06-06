243900d757a66afe1f1776760428b6a90ce683cf
Priyanka yadav <yadav.priyanka.m@gmail.com>
2024-06-06 20:43:01 +0530

    updated report

--- M_Project/cypress.config.js
+++ M_Project/cypress.config.js
@@ -3,6 +3,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   //video:true,
+  "reporter":"cypress-multi-reporters",
+  "reporterOptions":{
+    "reporterEnabled":"mochawesome",
+    "mochawsomeReporteroptions":{
+      "reportDir":"cypress/reports/mocha",
+      "quiet":true,
+      "overwrite":false,
+      "html":false,
+      "json":true
+    }
+  },
   e2e: {
     setupNodeEvents(on, config) {
       
